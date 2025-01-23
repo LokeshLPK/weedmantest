3fb20e0fb4b891ba2914a44ce94d7d1c49acce91
amitwaghmaretorinit <amit@torinit.com>
2025-01-23 16:50:22 +0530

    subpage removed

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -10,9 +10,7 @@ export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
-console.log({'projectId during build': projectId})
-console.log({'dataset during build': dataset})
-
+ 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
